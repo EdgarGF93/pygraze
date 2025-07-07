cd330dc5b931ea3d1b7523bf2e7048e6f3bd400f
Edgar <edgar.gutierrez-fernandez@esrf.fr>
2025-07-07 22:06:32 +0200

    first commit

---
+++ src/pygraze/__init__.py
@@ -0,0 +1,3 @@
+from .experiments.grazing import (  # noqa: F401
+    GrazingExperiment as Experiment,
+)
---
+++ src/pygraze/experiments/grazing.py
@@ -0,0 +1,5 @@
+class GrazingExperiment:
+
+    def __init__(self, name: str = None, experiment_id: str = None):
+        self.name = name
+        self.experiment_id = experiment_id
